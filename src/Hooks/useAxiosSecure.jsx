1b91cc9bb4ecc2607adaaee74d06f390f601b192
shrabonmolla <shrabonmolla007@gmail.com>
2025-12-19 17:59:16 +0600

    securing api call with interceptor in axiossccure

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,19 +1,48 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import useAuthHook from "./useAuthHook";
+import { useNavigate } from "react-router";
+
+const axiosSecure = axios.create({
+  baseURL: `http://localhost:3000`,
+});
 
 export default function useAxiosSecure() {
-  const axiosSecure = axios.create({
-    baseURL: `http://localhost:3000`,
-  });
+  const { user, logOut } = useAuthHook();
+  const navigate = useNavigate();
 
-  const { user } = useAuthHook();
   useEffect(() => {
-    axiosSecure.interceptors.response.use((config) => {
-      config.headers.Authorization = `Bearer ${user?.accessToken} `;
+    // intercept request
+
+    const reqInterceptor = axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
       return config;
     });
-  }, []);
+
+    // interceptor response
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log(error);
+
+        const statusCode = error.status;
+        if (statusCode === 401 || statusCode === 403) {
+          logOut().then(() => {
+            navigate("/authlayout/login");
+          });
+        }
+
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(reqInterceptor);
+      axiosSecure.interceptors.response.eject(resInterceptor);
+    };
+  }, [user, navigate, logOut]);
 
   return axiosSecure;
 }
